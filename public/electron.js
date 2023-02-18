daf3c7bbb1f6dbb341bbbb4249c779f19d221e21
Matsushoooo12 <matsush.oooo12@icloud.com>
2023-02-18 23:28:41 +0900

    first commit

---
+++ public/electron.js
@@ -0,0 +1,29 @@
+const {app, BrowserWindow} = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
+function createWindow(){
+    const mainWindow = new BrowserWindow({
+        widdth: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadURL(
+        isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+    )
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', function(){
+        if(BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function(){
+    if(process.platform !== 'darwin') app.quit()
+})
